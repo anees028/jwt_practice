8e145a74ee665991ea749d3e3ae3d85cd6537fce
anees028 <rehmananees028@gmail.com>
2025-02-13 15:11:30 +0100

    json parser applied to the application.

--- controllers/authController.js
+++ controllers/authController.js
@@ -8,9 +8,12 @@ module.exports.login_get = (req, res) => {
 }
 
 module.exports.signup_post = async (req, res) => {
+    const {name, password} = req.body;
     res.send('new signup');
 }
 
 module.exports.login_post = async (req, res) => {
+    const {name, password} = req.body;
+    console.log(name, password)
     res.send('user login');
 }
