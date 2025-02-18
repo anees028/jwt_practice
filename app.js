da6fdb1b146ae5ed8568e760a1fe5e1a04eee43b
anees50hertz <anees50hertz@gmail.com>
2025-02-18 12:02:14 +0100

    Setting and getting cookies.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/auth');
 const dotenv = require("dotenv");
+const cookieParser = require('cookie-parser')
 
 dotenv.config()
 
@@ -9,7 +10,8 @@ const app = express();
 
 // middleware
 app.use(express.static('public'));
-app.use(express.json())
+app.use(express.json());
+app.use(cookieParser());
 
 // view engine
 app.set('view engine', 'ejs');
@@ -29,3 +31,22 @@ mongoose.connect(dbURI)
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
 app.use(authRoutes);
+
+
+// Cookies  (Setting cookies)
+app.get('/set-cookies', (req, res) => {
+
+    // res.setHeader('Set-Cookies', 'newUser=true'),     //same step is below
+    res.cookie('newUser', false)
+    res.cookie('isEMployee', true, {maxAge: 1000*60*60*24, httpOnly:true})
+
+    res.send('you got the cookies.')
+})
+
+app.get('/read-cookies', (req, res)=>{
+    const cookies = req.cookies;
+    console.log(cookies)
+
+    res.json(cookies)
+})
+
